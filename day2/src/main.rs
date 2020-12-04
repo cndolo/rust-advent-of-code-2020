14b5e459555c2ad97fad082c142e94852f629477
Charmaine Ndolo <Charmaine.Ndolo@hu-berlin.de>
2020-12-04 15:26:05 +0100

    Second part of day 2 puzzle

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -9,8 +9,8 @@ fn read_password_file(file_path: &str) -> Vec<String> {
     reader.lines().filter_map(io::Result::ok).collect()
 }
 
-fn check_passwords_are_valid(list: &[String]) -> usize {
-    let mut valid_passwords: usize = 0;
+fn check_passwords_are_valid(list: &[String]) -> (usize, usize) {
+    let (mut valid_passwords, mut valid_passwords_second): (usize, usize) = (0, 0);
     for i in list {
         let mut tmp_pw = i.replace("-", ",");
         tmp_pw = tmp_pw.replace(":", ",");
@@ -23,9 +23,12 @@ fn check_passwords_are_valid(list: &[String]) -> usize {
         if is_valid_password(min, max, letter, password) {
             valid_passwords += 1;
         }
+        if is_valid_password_second_policy(min, max, letter, password) {
+            valid_passwords_second += 1;
+        }
     }
 
-    valid_passwords
+    (valid_passwords, valid_passwords_second)
 }
 
 fn is_valid_password(min: usize, max: usize, letter: char, password: &str) -> bool {
@@ -36,9 +39,18 @@ fn is_valid_password(min: usize, max: usize, letter: char, password: &str) -> bo
     false
 }
 
+fn is_valid_password_second_policy(pos1: usize, pos2: usize, letter: char, password: &str) -> bool {
+    if password.chars().nth(pos1).unwrap() == letter {
+        return !(password.chars().nth(pos2).unwrap() == letter)
+    } else if password.chars().nth(pos2).unwrap() == letter {
+        return true;
+    }
+    false
+}
 fn main() {
     let list = read_password_file("input.txt");
     println!("Read {} passwords", list.len());
     let valid_passwords = check_passwords_are_valid(&list);
-    println!("Found {} valid passwords.", valid_passwords);
+    println!("First policy {} valid passwords.", valid_passwords.0);
+    println!("Second policy {} valid passwords.", valid_passwords.1);
 }
